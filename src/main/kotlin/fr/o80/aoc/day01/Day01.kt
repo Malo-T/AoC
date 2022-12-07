7c1995dff73c0b189a4aacf350b5a8a3e814c7b2
Malo Toanen <malo.toanen@zenika.com>
2022-12-07 13:43:30 +0100

    day 1

--- src/main/kotlin/fr/o80/aoc/day01/Day01.kt
+++ src/main/kotlin/fr/o80/aoc/day01/Day01.kt
@@ -2,20 +2,19 @@ package fr.o80.aoc.day01
 
 class Day01 {
 
-    fun parse1(input: String): List<Int> {
-        TODO()
-    }
+    fun parse(input: String): List<List<Int>> =
+        input.split("\n\n")
+            .map { it.lines().map { line -> line.toInt() } }
 
-    fun part1(parsed: List<Int>): Int {
-        TODO()
-    }
+    fun part1(parsed: List<List<Int>>): Int =
+        parsed.maxOf { it.sum() }
 
-    fun parse2(input: String): List<Int> {
-        TODO()
-    }
+    fun part2(parsed: List<List<Int>>): Int =
+        parsed.map { it.sum() }
+            .sorted()
+            .reversed()
+            .take(3)
+            .sum()
 
-    fun part2(parsed: List<Int>): Int {
-        TODO()
-    }
 
 }
--- src/test/kotlin/fr/o80/aoc/day01/part2/input1.kt
+++
@@ -1,4 +0,0 @@
-package fr.o80.aoc.day01.part2
-
-const val result_d1_p2_1 = 2
-const val input_d1_p2_1 = """14"""
